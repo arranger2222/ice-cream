9aadb3579455f8c99efec5f8a569dd5448ef7fbe
Max Yuvkovetskiy <93490235+arranger2222@users.noreply.github.com>
2022-01-30 19:10:39 +0200

    Update top-btn.js

--- src/js/top-btn.js
+++ src/js/top-btn.js
@@ -5,7 +5,7 @@ $("#back-top").hide();
 // fade in #back-top
 $(function () {
 $(window).scroll(function () {
-if ($(this).scrollTop() > 1000) {
+if ($(this).scrollTop() > 700) {
 $('#back-top').fadeIn();
 } else {
 $('#back-top').fadeOut();
