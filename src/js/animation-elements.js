7926bb8b24ff289d2a06ff1256b86e413f29f713
ihor-kozyr <94694963+ihor-kozyr@users.noreply.github.com>
2022-02-01 11:17:14 +0200

    Анімування карток та кнопок в  about, advantages, contacts, products
    Зміни внесені за допомогою class="card4"

--- src/js/animation-elements.js
+++ src/js/animation-elements.js
@@ -1,3 +1,4 @@
+//Анимирование только Gallery
 const cards = document.querySelectorAll('.card');
 VanillaTilt.init(cards, {
     max: 15,
@@ -16,6 +17,7 @@ const card2s = document.querySelectorAll('.card2');
 VanillaTilt.init(card2s, {
     max: 15,
     glare: true,
+    "max-glare": 0.5,      // maximum "glare" opacity (1 = 100%, 0.5 = 50%)
     scale:  1.2,  // Масштабирование
     speed:  300,  // Скорость анимации в начале/в конце
     easing:  "cubic-bezier(.03,.98,.52,.99)", 
@@ -28,8 +30,23 @@ VanillaTilt.init(card2s, {
 });
 
 const card3s = document.querySelectorAll('.card3');
-VanillaTilt.init(card3, {
+VanillaTilt.init(card3s, {
     max: 10,
     glare: true,
 });
-
+//Анимирование в about, advantages, contacts, products
+const card4s = document.querySelectorAll('.card4');
+VanillaTilt.init(card4s, {
+    max: 9,
+    glare: true,
+    "max-glare": 0.5,      // maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+    scale:  1.1,  // Масштабирование
+    speed:  300,  // Скорость анимации в начале/в конце
+    easing:  "cubic-bezier(.03,.98,.52,.99)", 
+    gyroscope:  true,  // Нужен ли эффект при наклоне телефона/другого устройства
+    /* Максимальные и минимальные углы эффекта при наклоне устройства */
+    gyroscopeMinAngleX:  -45,
+    gyroscopeMaxAngleX:  45,
+    gyroscopeMinAngleY:  -45,
+    gyroscopeMaxAngleY:  45,
+});
